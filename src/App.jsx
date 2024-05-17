c7c54d2cb25bd771557d830f523ea7d6c52bd7c2
Rebekah Callari-Kaczmarczyk <rkaczmarczyk@phillies.com>
2024-05-17 11:43:47 -0400

    :art: run prettier and lint

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState, useEffect } from "react";
-import "./App.css";
-import { useFetch } from "./hooks";
-import { searchCollection } from "./network";
+import { useState, useEffect } from 'react';
+import './App.css';
+import { useFetch } from './hooks';
+import { searchCollection } from './network';
 
 function App() {
   const [isFetchingSearchResults, setIsFetchingSearchResults] = useState(false);
   const [searchResults, setSearchResults] = useState();
   const [searchResultsError, setSearchResultsError] = useState();
-  const {data: selectedArtwork, setPath: setSelectedArtworkApiLink} = useFetch();
+  const { data: selectedArtwork, setPath: setSelectedArtworkApiLink } = useFetch();
 
-  const queryTerm = "monet";
+  const queryTerm = 'monet';
 
   useEffect(() => {
     // Clear out former data
@@ -17,56 +17,54 @@ function App() {
     setSearchResults(undefined);
     setSearchResultsError(false);
 
-
     // Fetch data
     searchCollection(queryTerm)
-      .then((data) =>
-        {
-          // Update because data is successfully returned
-          setSearchResults(data);
-          setIsFetchingSearchResults(false);
-          setSearchResultsError(false);
-        }
-      )
-      .catch((error) =>
-        {
-          // Update because there was an error
-          setSearchResults(null);
-          setIsFetchingSearchResults(false);
-          setSearchResultsError(error);
-        }
-
-      );
+      .then((data) => {
+        // Update because data is successfully returned
+        setSearchResults(data);
+        setIsFetchingSearchResults(false);
+        setSearchResultsError(false);
+      })
+      .catch((error) => {
+        // Update because there was an error
+        setSearchResults(null);
+        setIsFetchingSearchResults(false);
+        setSearchResultsError(error);
+      });
   }, [queryTerm]);
 
   const handleMoreInfoButtonClick = (event) => {
     // Documentation for this API request: https://api.artic.edu/docs/#get-artworks-id
     const clickedArtworkApiLink = event.target.value;
-    setSelectedArtworkApiLink(clickedArtworkApiLink)
+    setSelectedArtworkApiLink(clickedArtworkApiLink);
   };
 
-  if (isFetchingSearchResults) return <h1>Loading...</h1>
-  if (searchResultsError) return <h1>{`There was an error: ${searchResultsError}`}</h1>
+  if (isFetchingSearchResults) return <h1>Loading...</h1>;
+  if (searchResultsError) return <h1>{`There was an error: ${searchResultsError}`}</h1>;
   return (
     <>
       <div>
-      <h1>{`Search Results for: ${queryTerm}`}</h1>
+        <h1>{`Search Results for: ${queryTerm}`}</h1>
         <div>
-        {searchResults &&
-          searchResults.map((artwork) => (
-            <ul key={artwork.id} >
-              <h3>{artwork.title}</h3>
-              <button value={artwork.api_link} onClick={handleMoreInfoButtonClick}>More Info</button>
-            <button onClick={() => {
-              window.open(
-              `https://www.artic.edu/artworks/${artwork.id}`,
-              '_blank' 
-            )}}>
-              Go to website
-            </button>
-            {selectedArtwork && selectedArtwork.id == artwork.id && <div dangerouslySetInnerHTML={{__html: selectedArtwork.description}}></div>}
-            </ul>
-          ))}
+          {searchResults &&
+            searchResults.map((artwork) => (
+              <ul key={artwork.id}>
+                <h3>{artwork.title}</h3>
+                <button value={artwork.api_link} onClick={handleMoreInfoButtonClick}>
+                  More Info
+                </button>
+                <button
+                  onClick={() => {
+                    window.open(`https://www.artic.edu/artworks/${artwork.id}`, '_blank');
+                  }}
+                >
+                  Go to website
+                </button>
+                {selectedArtwork && selectedArtwork.id == artwork.id && (
+                  <div dangerouslySetInnerHTML={{ __html: selectedArtwork.description }}></div>
+                )}
+              </ul>
+            ))}
         </div>
       </div>
     </>
--- src/hooks.js
+++ src/hooks.js
@@ -1,14 +1,17 @@
-import { useEffect, useState } from "react";
-import { fetchData } from "./network";
+import { useEffect, useState } from 'react';
+import { fetchData } from './network';
 
-export const useFetch = (initialPath=undefined) => {
-    const [path, setPath] = useState(initialPath);
-    const [data, setData] = useState();
-    const [error, setError] = useState();
+export const useFetch = (initialPath = undefined) => {
+  const [path, setPath] = useState(initialPath);
+  const [data, setData] = useState();
+  const [error, setError] = useState();
 
-    useEffect(() => {
-        if (path) fetchData(path).then(data => setData(data)).catch(error => setError(error))
-    }, [path])
+  useEffect(() => {
+    if (path)
+      fetchData(path)
+        .then((data) => setData(data))
+        .catch((error) => setError(error));
+  }, [path]);
 
-    return {data, error, setPath};
+  return { data, error, setPath };
 };
